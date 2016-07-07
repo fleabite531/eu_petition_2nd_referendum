0296c47195131ddf0199cd973ca7270f81938a6e
Alice Fleabite <fleabite531@gmail.com>
2016-07-07 18:24:32 +0100

    brought in old data file format. all.json now includes from start of when i was scraping and all.csv is all of this converted into csv

--- datatransform.py
+++ datatransform.py
@@ -1,14 +1,16 @@
 import ipdb
 
 from dateutil import parser
+import time
+import datetime
 
 import json
 
 import re
 
 
-inputfile = open("output", "r")
-outputfile = open("first_few_days.json", "a")
+inputfile = open("data/outputstruct", "r")
+outputfile = open("data/outputstruct.json", "a")
 
 sig_count_re = re.compile("[0-9,]+signatures")
 
@@ -22,13 +24,18 @@ for inputline in inputfile:
     sig_count = sig_count.replace(",", "")
     sig_count = sig_count.replace("signatures", "")
 
-    datestring = sig_count_raw.string[:sig_count_raw.start() - 3]
+    datestring = sig_count_raw.string[:sig_count_raw.start()]
 
-    timestamp = parser.parse(datestring)
+    numbers_re = re.compile("[0-9]+")
+    
+    time_list = numbers_re.findall(datestring)
+    
+    timestamp = datetime.datetime(*map(int, time_list[:-3]))
 
     outputfile.write(json.dumps({
         timestamp.isoformat() : sig_count
     }) + "\n")
+
 
 
 inputfile.close()
