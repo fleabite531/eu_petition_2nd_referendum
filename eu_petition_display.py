fb2034b77bb0563609ed71bdf807c6e5ba1f7487
Alice Fleabite <fleabite531@gmail.com>
2016-07-07 12:41:28 +0100

    added date range display options for graph

--- eu_petition_display.py
+++ eu_petition_display.py
@@ -1,3 +1,21 @@
+"""
+Displays data of timestamp against number of signatures
+
+
+TODO:
+    prettify graph:
+        add title and stuff [ ]
+        tidy up x axis labels [ ]
+        sort out whitespace around graph [ ]
+        add pointer and citation to when sigs got removed [ ]
+
+    add date range options [*]
+
+    investigate seaborn wrapper to make prettier [ ]
+
+
+"""
+
 import matplotlib.pyplot as plt
 import matplotlib
 import json
@@ -9,29 +27,45 @@ import argparse
 
 import ipdb
 
-filename = "all.json"
-
 
 argparser = argparse.ArgumentParser()
 
 argparser.add_argument("--jsonfile", help="json file to display, format is datestamp and sig_count",
-        default="all.json")
-argparser.add_argument("--from", help="optional field to display from specified datetime", 
+        default="data/all.json")
+argparser.add_argument("--from-time", help="optional field to display from specified datetime", 
         default=False)
-argparser.add_argument("--to", help="optional field to display to specified datetime",
+argparser.add_argument("--to-time", help="optional field to display to specified datetime",
         default=False)
 
 args = argparser.parse_args()
+
+from_time = to_time = None
+
+if args.from_time:
+    from_time = dateutil.parser.parse(args.from_time)
+if args.to_time:
+    to_time = dateutil.parser.parse(args.to_time)
+
 
 timestamps = []
 sig_counts = []
 
 with open(args.jsonfile, "r") as f:
     for jsonline in f:
-        timestamp, sig_count = json.loads(jsonline).popitem()
+        rawtimestamp, sig_count = json.loads(jsonline).popitem()
         
-        timestamps.append(dateutil.parser.parse(timestamp))
+        timestamp = dateutil.parser.parse(rawtimestamp)
+
+        if from_time and timestamp < from_time:
+            continue
+
+        if to_time and timestamp > to_time:
+            continue
+
+        timestamps.append(timestamp)
         sig_counts.append(int(sig_count.replace(",","")))
+
+
 
 
 matplotlibtimestamps = matplotlib.dates.date2num(timestamps)
@@ -42,7 +76,6 @@ plt.plot_date(x=matplotlibtimestamps, y=sig_counts, xdate=True, ydate=False,
         markersize=1)
 plt.ylim(ymin=0, ymax=4500000)
 
-# major_formatter = matplotlib.dates.DateFormatter('%d/%m/%y %h:%m:%s')
 major_formatter = matplotlib.dates.DateFormatter('%d/%m/%y')
 minor_formatter = matplotlib.dates.DateFormatter('%H')
 
